ceb18c96e24195983363d73e74e398ce66f747c9
Boluwatife Ayodele <waplurd1@gmail.com>
2021-05-19 23:28:32 +0100

    added image upload feature

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const user = require('./models/user');
 const app = express();
 const postRoute = require("./routes/post");
 const userRoute = require("./routes/user");
-
+const imageRoute = require("./routes/images")
 
 app.use(express.json());
 app.use("/post", postRoute);
 app.use("/user", userRoute);
+app.use("/image",imageRoute);
 
 
 module.exports = app
---
+++ routes/images.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const imageController = require("../controllers/image.controller");
+const imageUploader = require("../helpers/image-uploader");
+const checkAuth = require("../middlewares/check-auth");
+
+
+const router = express.Router();
+
+router.post("/upload", checkAuth.checkAuth, imageUploader.upload.single('images'), imageController.upload);
+module.exports = router;
