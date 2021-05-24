8245c89f526927c7faeadd881bce7f98a9c293fc
Boluwatife Ayodele <waplurd1@gmail.com>
2021-05-24 10:55:38 +0100

    rebuild for deployment

--- config/config.js
+++ config/config.js
@@ -3,28 +3,47 @@ require("dotenv").config();
 const { Sequelize } = require('sequelize')
 const { DB_NAME, USERNAME, PASS, HOST } = process.env
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+
+const Sequelize = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports =  new Sequelize(HOST, {
   host: HOST,
   dialect: 'mysql',
-})
+  operatorsAliases: false,
 
-testDbConnection = async () => {
-  try {
-    await sequelize.authenticate()
-    // sequelize.sync({ force: true })
-  } catch (error) {
-    console.error(error)
-  }
-}
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+});
 
-module.exports = { sq: sequelize, testDbConnection }
+// const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
+//   host: HOST,
+//   dialect: 'mysql',
+// })
 
-// module.exports = {
-//     HOST: process.env.HOST,
-//     USER: process.env.USERNAME,
-//     PASSWORD: process.env.PASS,
-//     DB: process.env.DB_NAME
-//   };
+// testDbConnection = async () => {
+//   try {
+//     await sequelize.authenticate()
+//     // sequelize.sync({ force: true })
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
+
+// module.exports = { sq: sequelize, testDbConnection }
+
+// // module.exports = {
+// //     HOST: process.env.HOST,
+// //     USER: process.env.USERNAME,
+// //     PASSWORD: process.env.PASS,
+// //     DB: process.env.DB_NAME
+// //   };
 
 
 
